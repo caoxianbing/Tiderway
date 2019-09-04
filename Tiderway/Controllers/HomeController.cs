b1410960477f011dcfc4955e3431fadc260eb3ba
1151833345 <1151833345@qq.com>
2019-09-04 19:41:01 +0800

    my first commit

---
+++ Tiderway/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tiderway.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult BanPai()
+		{
+			return View();
+		}
+
+		public ActionResult School()
+		{
+			return View();
+		}
+
+		public ActionResult Health()
+		{
+			return View();
+		}
+		public ActionResult SchoolHealth()
+		{
+			return View();
+		}
+
+		public ActionResult GNC()
+		{
+			return View();
+		}
+	}
+}
